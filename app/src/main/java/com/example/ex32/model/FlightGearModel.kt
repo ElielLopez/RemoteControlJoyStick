a59db3987efd026787fcc6bec72517e117604335
ElielLopez <elielsw1@gmail.com>
2021-06-20 12:18:11 +0300

    creating sendCommand method instead of 4

--- app/src/main/java/com/example/ex32/model/FlightGearModel.kt
+++ app/src/main/java/com/example/ex32/model/FlightGearModel.kt
@@ -1,6 +1,7 @@
 package com.example.ex32.model
 
 //import com.example.ex32.vm.FlightGearViewModel
+import java.io.IOException
 import java.io.PrintWriter
 import java.net.Socket
 import java.util.concurrent.BlockingDeque
@@ -28,31 +29,57 @@ object FlightGearModel {
 
     fun rudderChanged(rudder: Float){
         //println("Model Rudder is: $rudder")
-        dispatchQueue.put(Runnable {
-            out.print("set /controls/flight/rudder $rudder \r\n")
-            out.flush()
-        })
+//        dispatchQueue.put(Runnable {
+//            out.print("set /controls/flight/rudder $rudder \r\n")
+//            out.flush()
+//        })
+        sendCommand("rudder", rudder)
     }
 
     fun throttleChanged(throttle: Float) {
         //println("Model throttle is: $throttle")
-        dispatchQueue.put(Runnable {
-            out.print("set /controls/engines/current-engine/throttle $throttle \r\n")
-            out.flush()
-        })
+//        dispatchQueue.put(Runnable {
+//            out.print("set /controls/engines/current-engine/throttle $throttle \r\n")
+//            out.flush()
+//        })
+        sendCommand("throttle", throttle)
     }
 
     fun elevatorChanged(elevator: Float){
-        dispatchQueue.put(Runnable {
-            out.print("set /controls/flight/elevator $elevator \r\n")
-            out.flush()
-        })
+//        dispatchQueue.put(Runnable {
+//            out.print("set /controls/flight/elevator $elevator \r\n")
+//            out.flush()
+//        })
+        sendCommand("elevator", elevator)
     }
 
     fun aileronChanged(aileron: Float){
-        dispatchQueue.put(Runnable {
-            out.print("set /controls/flight/aileron $aileron \r\n")
-            out.flush()
-        })
+//        dispatchQueue.put(Runnable {
+//            out.print("set /controls/flight/aileron $aileron \r\n")
+//            out.flush()
+//        })
+        sendCommand("aileron", aileron)
+    }
+
+    fun sendCommand(feature: String,value: Float){
+        var command = ""
+
+        if(feature == "rudder") {
+            command = "set /controls/flight/rudder $value \r\n"
+        }else if(feature == "throttle") {
+            command = "set /controls/engines/current-engine/throttle $value \r\n"
+        }else if(feature == "elevator") {
+            command = "set /controls/flight/elevator $value \r\n"
+        }else if(feature == "aileron") {
+            command = "set /controls/flight/aileron $value \r\n"
+        }
+        try{
+            dispatchQueue.put(Runnable {
+                out.print(command)
+                out.flush()
+            })
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
     }
 }
